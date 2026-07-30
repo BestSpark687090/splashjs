a38e40bdb604ba967938e984a3749e52627b0faa
BestSpark687090 <bestspark687090@gmail.com>
2026-07-30 18:03:12 -0500

    inital commit- aka making it

---
+++ README.md
@@ -0,0 +1,6 @@
+# SplashJS
+
+So my goal for this project is to make a splash-text-type thing.
+
+Y'know how Minecraft has splash text? Yeah- something like that.
+
---
+++ index.js
@@ -0,0 +1,15 @@
+// First thing's first. Make an export-thing
+/**
+ * Here's how it looks:
+ * splashjs({
+ *  preset: '', // OVERWRITES ALL OTHER OPTIONS I GUESS
+ *  target: "", // Query Selector, eg #icon
+ *  splashes: ["Splash 1", "Splash 2", "etc splashes"], // duh
+ *  color: "", // Hex code or RGB value thingy
+ *  font: "" // Optional, uses the default font
+ *  // obviously im gonna add more as this goes on
+ * })
+ */
+function splashjs(options){
+    console.log("Hello! You've reached SplashJS. heres the options",options)
+}
